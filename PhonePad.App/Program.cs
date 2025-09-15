dacafaa9983ad7924d6e027c1a0a9f4848bc30e7
mandipdhakal <m.dhakal@hydroneo.net>
2025-09-15 17:20:19 +0700

    first commit

---
+++ PhonePad.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Net.Turing.Phone;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(OldPhonePadUtility.Process("33#"));                // E
+        Console.WriteLine(OldPhonePadUtility.Process("227*#"));              // B
+        Console.WriteLine(OldPhonePadUtility.Process("4433555 555666#"));    // HELLO
+        Console.WriteLine(OldPhonePadUtility.Process("8 88777444666*664#")); // TURING
+    }
+}
